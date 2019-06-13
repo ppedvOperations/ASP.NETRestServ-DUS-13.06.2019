2356120a2f7d004505b984eaa61ae8d00887c487
Ali Ilci <Ali.Ilci@gmx.de>
2019-06-13 12:15:59 +0200

    Initial

---
+++ SdtWebApi/Sdt.Web.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sdt.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SdtWebApi/Sdt.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sdt.Web.Api.WebApiApplication" Language="C#" %>
